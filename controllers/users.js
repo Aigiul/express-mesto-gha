e235b760d39e19910460ccc2061306c0e0adc4be
Aigiul <vivat_narimanov@mail.ru>
2023-01-01 21:39:20 +0300

    Test 9

--- controllers/users.js
+++ controllers/users.js
@@ -108,7 +108,7 @@ module.exports.updateAvatar = (req, res, next) => {
     {
       new: true,
       runValidators: true,
-      upsert: false,
+      upsert: true,
     },
   )
   .orFail(() => {
