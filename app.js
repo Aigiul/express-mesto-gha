71e6212f54fae0406c2d270170a87acaeb34a8cf
Aigiul <vivat_narimanov@mail.ru>
2023-01-10 20:58:59 +0300

    Логи

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { INTERNAL_SERVER_ERROR } = require('./errors/status-codes');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/not-found-error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 
@@ -16,6 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(MONGO_URL);
+
+app.use(requestLogger); // подключаем логгер запросов
 
 // роуты не требующие авторизации
 app.post('/signin', celebrate({
@@ -46,7 +49,11 @@ app.use('*', (req, res, next) => {
   next(new NotFoundError('Запрашиваемый путь не найден'));
 });
 
-app.use(errors());
+app.use(errorLogger); // подключаем логгер ошибок
+
+app.use(errors()); // обработчик ошибок celebrate
+
+// централизованный обработчик ошибок
 
 app.use((err, req, res, next) => {
   if (err.statusCode) {
